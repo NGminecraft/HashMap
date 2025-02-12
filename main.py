19fa18cfa2c515256d16242efe6783567a25180f
NGminecrafter <grahanic001@alpinesd.org>
2025-02-11 21:04:10 -0700

    Blurb

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+class HashMap:
+    def __init__(self):
+        self.array = []
+        self.size = 0
 
+        # Not really meant to be changed, for debugging
+
+    def add(self):
+        # 
+        pass
