f4d1c4b59d047a446ed1f1953a494d861588c769
NGminecraft <minecrafterng399@gmail.com>
2025-02-22 22:34:16 -0700

    It's all broken

--- students_code.py
+++ students_code.py
@@ -1,5 +1,5 @@
 import numpy as np
-import threading
+import multiprocessing
 import warnings
 import string
 from random import randint, choice
@@ -53,7 +53,7 @@ class HashMap:
         print(f"Updating formulas for {len(self.array)} items")
         self.array.sort(key=lambda x: self.l1Hash(x.word))
         self.assert_safe()
-        self.polyThread = threading.Thread(target=self.calculate_regression, args=(self.index_array, ))
+        self.polyThread = multiprocessing.Process(target=self.calculate_regression, args=(self.index_array, ))
         self.polyThread.start()
         self.threadActive = True
 
@@ -69,95 +69,114 @@ class HashMap:
             return np.log10(arr)
 
         def create_polynomial(unformatted_indices, expected_indices, backup_letters=0):
-            if len(unformatted_indices) == 1:
-                return lambda x: expected_indices[0]
-            elif len(set(unformatted_indices)) == 1:
-                return lambda x: expected_indices[0]
             indices = format_array(unformatted_indices)
-            power = 0
-            while power <= 10:
-                try:
-                    coes = np.polyfit(indices, expected_indices, power)
-                    polynomial =  np.poly1d(coes)
-                except np.RankWarning:
-                    power += 1
-                else:
-                    # Ok now we need to check and make sure the polynomial actually fits well enough
-                    for each_key, expected_output in zip(indices, expected_indices):
-                        returned = polynomial(each_key)
-                        # If any fail, we know that we need to refit
-                        error = abs(returned-expected_output)
-                        if error > 0.49: # I originally just used round(returned) != expected_output, but was running into what I think is floating point error
-                            power+=1
-                            break
+            match len(unformatted_indices):
+                case 1:
+                     # If there is only one item, return it's index
+                    return lambda x: expected_indices[0]
+                case 2:
+                    
+                    # I can actually do this, and this is probably a bit faster
+                    slope = (expected_indices[1]- expected_indices[0])/(indices[1]-indices[0])
+
+                    intercept = expected_indices[0] - (slope * indices[0])
+                    
+                    def linear_function(x, m=slope, b=intercept):
+                        return m * x + b
+                    return linear_function
+                case _:
+                    power = 0
+                    MAX_POWER = 10
+                    error_arrays = [None for _ in range(MAX_POWER)]
+                    num_longest_continous = [None for _ in range(MAX_POWER)]
+                    while power <= MAX_POWER:
+                        coes = np.polyfit(indices, expected_indices, power)
+                        polynomial =  np.poly1d(coes)
+                        # Ok now we need to check and make sure the polynomial actually fits well enough
+                        for each_key, expected_output in zip(indices, expected_indices):
+                            returned = polynomial(each_key)
+                            # If any fail, we know that we need to refit
+                            error = abs(returned-expected_output)
+                            if error > 0.49: # I originally just used round(returned) != expected_output, but was running into what I think is floating point error
+                                power+=1
+                                break # TODO, allow functions to map to only part of the data
+                        else:
+                            # They all passed, so this polynomial works
+                            return polynomial
                     else:
-                        # They all passed, so this polynomial works
-                        return polynomial
-            else:
-                # If we get here, that's bad. We couldn't get a polynomial to reliably work
-                # Further data processing is needed.
+                        # If we get here, that's bad. We couldn't get a polynomial to reliably work
+                        # Further data processing is needed.
 
-                # Probably repeat the same algorithm, but chunk the words by the first digit of the index, instead of just word legnth
+                        # Probably repeat the same algorithm, but chunk the words by the first digit of the index, instead of just word legnth
 
-                idx_start = 0
-                current_first_char = str(unformatted_indices[0])[backup_letters]
-                secondary_function_list = []
-                first_digits = [int(current_first_char)]
+                        idx_start = 0
+                        current_first_char = str(unformatted_indices[0])[backup_letters]
+                        secondary_function_list = []
+                        first_digits = [int(current_first_char)]
 
-                for i in range(1,len(unformatted_indices)):
-                    if str(unformatted_indices[i])[backup_letters] != current_first_char:
-                        func = create_polynomial(unformatted_indices[idx_start:i], expected_indices[idx_start:i], backup_letters+1)
-                        secondary_function_list.append(func)
+                        for i in range(1,len(unformatted_indices)):
+                            if str(unformatted_indices[i])[backup_letters] != current_first_char:
+                                func = create_polynomial(unformatted_indices[idx_start:i], expected_indices[idx_start:i], backup_letters+1)
+                                secondary_function_list.append(func)
 
-                        idx_start = i
-                        current_first_char = str(unformatted_indices[i])[backup_letters]
-                        first_digits.append(int(current_first_char))
-                else:
-                    func = create_polynomial(unformatted_indices[idx_start:], expected_indices[idx_start:], backup_letters+1)
-                    secondary_function_list.append(func)
-                
-                if backup_letters%2 == 0 and first_digits[0] >= 6:
-                    normalizer = -6 # Only the first digit of each ascii letter can be shorted
-                    # It starts at A, 65, but we can have a 70, and thus can't minus 6 if were looking at the second digit
-                else:
-                    normalizer = 0
+                                idx_start = i
+                                current_first_char = str(unformatted_indices[i])[backup_letters]
+                                first_digits.append(int(current_first_char))
+                        else:
+                            func = create_polynomial(unformatted_indices[idx_start:], expected_indices[idx_start:], backup_letters+1)
+                            secondary_function_list.append(func)
+                        
+                        if backup_letters%2 == 0 and first_digits[0] >= 6:
+                            normalizer = -6 # Only the first digit of each ascii letter can be shorted
+                            # It starts at A, 65, but we can have a 70, and thus can't minus 6 if were looking at the second digit
+                        else:
+                            normalizer = 0
 
-                final_function_list = [None for _ in range(first_digits[-1]+normalizer+1)]
-                for d, f in zip(first_digits, secondary_function_list):
-                    final_function_list[d+normalizer] = f
+                        final_function_list = [None for _ in range(first_digits[-1]+normalizer+1)]
+                        for d, f in zip(first_digits, secondary_function_list):
+                            final_function_list[d+normalizer] = f
 
                 
-                def returned_function(x, f_list=final_function_list, num_digits=backup_letters, norm=normalizer):
-                    main_idx = round(10**x)
-                    first_digit = int(str(main_idx)[num_digits])
-                    funct = f_list[first_digit+norm]
-                    funct_result = funct(x)
-                    if type(funct_result) is tuple:
-                        value = funct_result[0]
-                        steps = funct_result[1] + 1
-                    else:
-                        value = funct_result
-                        steps = 1
-                    return value, steps
-                
-                return returned_function
+                        def returned_function(x, f_list=final_function_list, num_digits=backup_letters, norm=normalizer):
+                            main_idx = round(10**x)
+                            first_digit = int(str(main_idx)[num_digits])
+                            funct = f_list[first_digit+norm]
+                            funct_result = funct(x)
+                            if type(funct_result) is tuple:
+                                value = funct_result[0]
+                                steps = funct_result[1] + 1
+                            else:
+                                value = funct_result
+                                steps = 1
+                            return value, steps
+                        
+                        return returned_function
         
+        def single_instance(idx, lst_to_use, offset):
+            # The indexing in the primary array is the offset (all the items in the lists before this one) + the result of the function
+            
+            match len(lst_to_use):
+                case 0:
+                    self.function_array[idx] = None
+                case 1:
+                    def return_const(x, const=offset):
+                        """A lambda didn't work for some reason"""
+                        return const
+                
+                    self.function_array[idx] = return_const # Just return the offset if everything works
+                case _:
+                    expected_indices = [i for i in range(offset, offset+len(lst_to_use))] # This forgoes needing to manually add, or change the polynomial
+                    
+                    poly = create_polynomial(lst_to_use, expected_indices)
+
+                    self.function_array[idx] = poly
+
 
         # lst is a list of lists. Each item in the list is a list of each words hash, split by word size
         # The get will go into the function array at the equivalent index array index, and then call it
-        for main_list_index, secondary_list in enumerate(lst):
-            # The indexing in the primary array is the offset (all the items in the lists before this one) + the result of the function
-            offset = sum(len(i) for i in lst[:main_list_index])
-            if len(secondary_list) == 0:
-                self.function_array[main_list_index] = None
-                continue
-            elif len(secondary_list) == 1:
-                self.function_array[main_list_index] = lambda x: offset # If there is only one item, we can just return the index
-                continue
-            expected_indices = [i for i in range(offset, offset+len(secondary_list))] # This forgoes needing to manually add, or change the polynomial
-            poly = create_polynomial(secondary_list, expected_indices)
+        with multiprocessing.Pool(10) as p:
+            p.map(single_instance, zip(range(len(lst)), lst, [sum(lst[:i]) for i in range(len(lst))]))
 
-            self.function_array[main_list_index] = poly
     
     def soft_insert(self, item, count=1):
         itemIndex = self.l1Hash(item)
@@ -186,7 +205,7 @@ class HashMap:
 
             
     def words_in(self, words):
-        unique_words = list(set(words))
+        unique_words = sorted(list(set(words)))
         
         for item in unique_words:
             self.soft_insert(item, 0)
@@ -228,7 +247,7 @@ class HashMap:
             
             result = funct(scaled_index)
             if type(result) is tuple:
-                number_of_steps = result[1] + 2
+                number_of_steps = result[1] + 2 # One for the function array lookup, then the main array lookup
                 value_to_check = round(result[0])
             else:
                 number_of_steps = 2
@@ -261,7 +280,9 @@ if __name__ == "__main__":
     def generate_random_word():
         return ''.join([choice(string.ascii_lowercase) for _ in range(randint(1, 15))])
         
-    inwords = [generate_random_word() for _ in range(1000000)]
+    
+    inwords = ["a", "a", "as", "at"]
+    #inwords = [generate_random_word() for _ in range(1000)]
     #inwords = ['g', 'n', 'n', 'hp', 'ij', 'md', 'ra', 'so', 'ty', 'xu', 'drd', 'fhj', 'gyg', 'hih', 'mfk', 'pae', 'umc', 'xfk', 'zee', 'cxou', 'iwld', 'pdiw', 'zovk', 'clyeb', 'efjsw', 'gxvwc', 'wjoaa', 'yxxut', 'zxrnn', 'etmlxo', 'fthzoy', 'ichyvk', 'jenazu', 'nauwew', 'noimfc', 'bvvxnxy', 'cjemair', 'etqdcxt', 'hqwdqwy', 'thlmfrt', 'busivlqg', 'cfiypojm', 'dygpsqae', 'dzmqapfz', 'gzzhtrfz', 'ijikhyik', 'iwcejujv', 'jeviteai', 'wacbjbgu', 'jsnljcsbl', 'wynnqimrf', 'zajxxsoyl', 'lbwrppygrf', 'nceakmbixb', 'pkikkfxwlq', 'pouzguexyb', 'rxeneqraeg', 'scaqrxfnbl', 'slxybsnqjg', 'vdqrmlhazb', 'ypalccnbqb', 'cnwkpgoqybz', 'jmlmrywfhfx', 'jrsqrmtapse', 'kpulqqoowke', 'ldutizxiwad', 'ndvyrivxgdb', 'vbvjlifparc', 'dhjklzdazgpg', 'irgerzyfassi', 'reahnbgvkpro', 'ucokdsosmeeo', 'xinmxqjbweik', 'aaeuxpgyuoxcl', 'bhwcmrlyngjwa', 'ctavuaziyaafd', 'ddajvmfhjdpqv', 'drrslvcboezlc', 'hdpptoamcjgtr', 'kmqvqmzowbknv', 'liyqlbuxveadq', 'ydmtegpfhqiay', 'dcvlmlogruamud', 'dyzavdxmywmczn', 'edureokkyvvddv', 'fredpmyenviqdm', 'fznnqbfracwrsb', 'gyptnhcqtxfjwf', 'hhhemhumvpxgxo', 'ivngvcmibhedvo', 'nsxfyebfbywddn', 'ponrfhqorynrfe', 'pqhowqpnwzurse', 'stfwtfvprikmjl', 'udctpexupkbxdz', 'hgptibmszdbkaaf', 'rhcxvbggscymcyf', 'xkiowecbuawlwbt', 'yvefzsvpqbjqrlt', 'zmfvryuuvkzsfki']
     words_in(inwords)
     finished = True
